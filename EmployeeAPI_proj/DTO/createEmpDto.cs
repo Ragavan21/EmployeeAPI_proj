5096d5ce79c330a72d2e72179d7a3ad54f565bd6
Ragav21 <ragavanragavan4753@gmail.com>
2026-05-29 00:31:18 +0530

    adding DTO model

--- EmployeeAPI_proj/DTO/createEmpDto.cs
+++ EmployeeAPI_proj/DTO/createEmpDto.cs
@@ -2,5 +2,14 @@
 {
     public class createEmpDto
     {
+        public string EmpName { get; set; }
+        public string Email { get; set;}
+        public string Phno { get; set; }
+        public string AddressId { get; set; }
+        public Decimal Salary { get; set; }
+        public int DepartmentID { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public DateTime createdDate { get; set; }
+
     }
 }
